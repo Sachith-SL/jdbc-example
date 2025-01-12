a71b1757de1e1d7f070ad8451667a84a789dcc29
Sachith-SL <itsmesachith@gmail.com>
2025-01-12 20:13:44 +0530

    add preparedStatement

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,16 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
         System.out.println("Hello world!");
-        String sql = "select name from t_company tc where id=1";
+        String query = "select name from t_company tc where id=?";
 
         String dbUrl ="jdbc:mysql://localhost:3306/bookmedb";
         String userName = "root";
         String password = "SC92@mysql";
 
         Connection con = DriverManager.getConnection(dbUrl,userName,password);
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(sql);
+        PreparedStatement st = con.prepareStatement(query);
+        st.setInt(1,1);
+        ResultSet rs = st.executeQuery();
         rs.next();
         System.out.println(rs.getString(1));
         con.close();
